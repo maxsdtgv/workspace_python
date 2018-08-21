4c27cfa01482a83cfbfa105c6fc2d436612d7721
mvysochinenko <mvysochinenko@sequans.com>
2018-08-21 18:48:49 +0300

    threads for all uarts

--- ncat_ssl/ncat_ssl.py
+++ ncat_ssl/ncat_ssl.py
@@ -36,7 +36,7 @@ uart2_console_speed = 115200
 key_file_name = "rootCA.key"
 pem_file_name = "rootCA.pem"
 
-ssl_server = "172.17.57.221"
+ssl_server = "172.17.57.243"
 
 r = '\r'
 n = '\n'
@@ -53,6 +53,7 @@ comm_9 = 'AT+SQNSS' + r
 comm_10 = '' + r
 comm_11 = 'AT+SQNSI' + r
 comm_12 = 'cbe"netstat"' + r
+comm_13 = 'AT^RESET' + r
 
 proc1 = ''
 proc2 = ''
@@ -61,7 +62,9 @@ proc4 = ''
 proc5 = ''
 proc6 = ''
 proc7 = ''
-line_to_send = ''
+line_to_send_console = ''
+line_to_send_at0 = ''
+line_to_send_at1 = ''
 
 def timest():
 	ts = time.time()
@@ -70,22 +73,25 @@ def timest():
 	return st
 
 def from_UE(ti, ts, tchannel, tprintout, check_response):
-	global queue_console
+	global queue_at0
+	global queue_at1
+	global queue_console	
+
 	matchFound = 0
 	lineslist = []
 	for tu in range(0, ti):
 		time.sleep(1)
 		if tchannel == 'channel0_at':
-			lineslist = channel0_at.readlines()
-
+			while not queue_at0.empty():
+				lineslist.append(queue_at0.get())
+				#lineslist+=queue_at0.get()
 		if tchannel == 'channel1_at':
-			lineslist = channel1_at.readlines()
+			while not queue_at1.empty():
+				lineslist.append(queue_at1.get())
 
 		if tchannel == 'channel2_console':
 			while not queue_console.empty():
 				lineslist.append(queue_console.get())
-
-		#print('lineslist='+str(lineslist))
 
 		for line in lineslist:
 			line = line.decode().replace('\r', '').replace('\n', '')
@@ -95,6 +101,7 @@ def from_UE(ti, ts, tchannel, tprintout, check_response):
 				if line.find(ts) != -1:
 					matchFound = 1
 					break
+		lineslist = []
 		if matchFound == 1:
 			break
 	if check_response == 1 and matchFound == 0:
@@ -103,23 +110,18 @@ def from_UE(ti, ts, tchannel, tprintout, check_response):
 	return matchFound
 
 def to_UE(channel, sty):
-	global line_to_send
+	global line_to_send_at0	
+	global line_to_send_at1	
+	global line_to_send_console
+
 	if channel == 'channel0_at':
-		channel0_at.write(comm_1.encode())
-		time.sleep(.2)
-		channel0_at.readlines()
-		time.sleep(.2)
-		channel0_at.write(sty.encode())
+		line_to_send_at0 = sty
 
 	if channel == 'channel1_at':
-		channel1_at.write(comm_1.encode())
-		time.sleep(.2)
-		channel1_at.readlines()
-		time.sleep(.2)
-		channel1_at.write(sty.encode())
+		line_to_send_at1 = sty
 
 	if channel == 'channel2_console':
-		line_to_send = sty
+		line_to_send_console = sty
 	return
 
 def gotoPSPM(waitPSPM):
@@ -158,7 +160,7 @@ def channelDetection():
 	print('\r\n')		
 	sys.stdout.write('[' + timest() + '] ')
 	print('======================= Detecting channels ==============================')
-	time.sleep(8)
+	time.sleep(9)
 	to_UE('channel0_at',comm_2)  # ============== send ATE
 	from_UE(5, '', 'channel0_at', 0, 0)
 
@@ -169,7 +171,7 @@ def channelDetection():
 		print('!    Error. AT channel does not responce')
 		sys.exit()
 
-	time.sleep(5)
+	time.sleep(9)
 	to_UE('channel2_console',comm_1)  # ============== send enter
 	if from_UE(5, '->', 'channel2_console', 0, 1) == 1:
 		print('     Console channel detected `->` found')	
@@ -178,16 +180,37 @@ def channelDetection():
 		sys.exit()
 
 def queue_channel(out, nqueue):
-	global line_to_send
+	global line_to_send_at0
+	global line_to_send_at1		
+	global line_to_send_console
+
 	while True:	#	nqueue.put(line)
+		if out == channel0_at:
+			for line in iter(out.readline, b''):
+				nqueue.put(line.replace('\r','').replace('\n',''))
+				log_at0.write('[' + timest() + ']' + ' ' + line)
+			if line_to_send_at0 != '':
+				time.sleep(.1)
+				out.write(line_to_send_at0)
+				line_to_send_at0 = ''
+
+		if out == channel1_at:
+			for line in iter(out.readline, b''):
+				nqueue.put(line.replace('\r','').replace('\n',''))
+				log_at1.write('[' + timest() + ']' + ' ' + line)				
+			if line_to_send_at1 != '':
+				time.sleep(.1)
+				out.write(line_to_send_at1)
+				line_to_send__at1 = ''
+
 		if out == channel2_console:
 			for line in iter(out.readline, b''):
 				nqueue.put(line.replace('\r','').replace('\n',''))
-
-			if line_to_send != '':
+				log_console.write('[' + timest() + ']' + ' ' + line)
+			if line_to_send_console != '':
 				time.sleep(.1)
-				out.write(line_to_send)
-				line_to_send = ''	
+				out.write(line_to_send_console)
+				line_to_send_console = ''	
 
 def queue_output(out, nqueue):
 	for line in iter(out.readline, b''):
@@ -225,12 +248,12 @@ def ncat_close_sockets():
 	       # for tls
 		to_UE('channel0_at',comm_9)  # send at+sqnss
 		from_UE(5, 'OK', 'channel0_at', 1, 1)
-
+		time.sleep(.1)
 		for i in range(1,7):         # ============== Open sockets for tls
 			spec_comm = 'AT+SQNSH=' + str(i) + r
 			to_UE('channel0_at',spec_comm)  
 			from_UE(5, 'OK', 'channel0_at', 1, 1)		
-
+			time.sleep(.1)
 	#====================================================================================================================
 
 def ncat_info():
@@ -357,24 +380,55 @@ try:
 	
 	print('\r\n')		
 	sys.stdout.write('[' + timest() + '] ')
+	print('======================= Log files creation')
+	tst = time.time()
+	name_time = datetime.datetime.fromtimestamp(tst).strftime('%Y-%m-%d_%H:%M:%S')
+	log_console = open(name_time+'_console.txt', 'w')
+	log_console.write('Log from console\r\n')
+	print ('     Log file for console is created - ' + name_time + '_console.txt')
+	log_at0 = open(name_time+'_at0.txt', 'w')
+	log_at0.write('Log from AT0\r\n')
+	print ('     Log file for AT0 is created - ' + name_time+'_at0.txt')
+	log_at1 = open(name_time+'_at1.txt', 'w')
+	log_at1.write('Log from AT1\r\n')
+	print ('     Log file for AT1 is created - ' + name_time+'_at1.txt')
+
+	print('\r\n')		
+	sys.stdout.write('[' + timest() + '] ')
 	print('=================== Threads for uart readings')
 
+	print('     Create queue ...')
 	queue_console = Queue.Queue()
+	queue_at0 = Queue.Queue()
+	queue_at1 = Queue.Queue()
+
+	print('     Create threads ...')
+	thread_at0 = threading.Thread(target=queue_channel, args=(channel0_at, queue_at0))
+	thread_at1 = threading.Thread(target=queue_channel, args=(channel1_at, queue_at1))		
 	thread_console = threading.Thread(target=queue_channel, args=(channel2_console, queue_console))
+
+	thread_at0.daemon = True # thread dies with the program
+	thread_at1.daemon = True # thread dies with the program
 	thread_console.daemon = True # thread dies with the program
-	thread_console.start()
-	print('Done.')
+
+	print('     Start threads ...')
+	thread_at0.start()
+	thread_at1.start()	
+	thread_console.start()	
+
+	print('     Done.')
 	print('============================================')
-
-	
-
 
 #====================================================================================================================
 
 #====================================================================================================================
 #	Check for key and pem files
 #====================================================================================================================
+
 	if proto == 0:
+		print('\r\n')		
+		sys.stdout.write('[' + timest() + '] ')
+		print('=================== Check for key and pem files')		
 		file_rootCA_key = pathlib.Path("./" + key_file_name)
 		if file_rootCA_key.is_file():
 			print('     Private key = ' + str(file_rootCA_key) + ' found!')   # file exists
@@ -390,7 +444,13 @@ try:
 			sys.exit()	
 #====================================================================================================================
 
-
+	print('\r\n')		
+	sys.stdout.write('[' + timest() + '] ')
+	print('=================== Resetting the board ...')
+	to_UE('channel0_at',comm_13)  # send at^reset
+	if from_UE(15, '+SYSSTART', 'channel0_at', 0, 1) == 1:
+		print('Expected response +SYSSTART is found!')
+	
 	channelDetection()
 
 #====================================================================================================================
@@ -413,6 +473,7 @@ try:
 #====================================================================================================================
 #	AT commands for NCAT SSL prepare
 #====================================================================================================================
+	ncat_info()
 	if proto == 0:
 		print('\r\n')		
 		sys.stdout.write('[' + timest() + '] ')
@@ -443,15 +504,22 @@ try:
 		print('\r\n')		
 		sys.stdout.write('[' + timest() + '] ')
 		print('======================= NCAT SSL/DTLS preparation ====================')
-		to_UE('channel0_at',comm_8)
+		to_UE('channel0_at',comm_8)          #AT+SQNSPCFG=1,2,"0x8C;0x8D;0xAE;0xAF",,,,,"AABC3BDFDE2526E815D76A22A364BA76641D3360A4A5FBEA9db8bed55d406982","Client_identity"
 		from_UE(5, 'OK', 'channel0_at', 1, 1)
 		print('============================================')
 		#==========================Next the same for TLS and DTLS ========================================
-		print('=================== Secure Socket Configuration =========================')
+
+	print('=================== Secure Socket Configuration =========================')
 	for i in range(1,7):         # ============== Configure Secure Socket Configuration AT+SQNSSCFG=i,1,1
 		spec_comm = 'AT+SQNSSCFG=' + str(i) + ',1,1' + r
 		to_UE('channel0_at',spec_comm)  
 		from_UE(5, 'OK', 'channel0_at', 1, 1)	
+
+	print('=================== Socket Configuration =========================')
+	for i in range(1,7):         # ============== Configure Secure Socket Configuration AT+SQNSSCFG=i,1,1
+		spec_comm = 'AT+SQNSCFG=' + str(i) + ',1,300,0,600,5' + r
+		to_UE('channel0_at',spec_comm)  
+		from_UE(5, 'OK', 'channel0_at', 1, 1)
 	print('============================================')
 #====================================================================================================================
 
@@ -510,10 +578,16 @@ finally:
 	print('\r\n')		
 	sys.stdout.write('[' + timest() + '] ')	
 	print('===================== Finalize ============================')
+	print('         Close files ...')
+	log_console.close()
+	log_at0.close()
+	log_at1.close()	
 	print('         Close uarts ...')
 	channel0_at.close()
 	channel1_at.close()	
 	channel2_console.close()
+
 	print('         Close subpocesses ...')
 	ssl_close_server()
+
 	print('         Done.')
